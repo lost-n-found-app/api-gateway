da7c15e6e8324f2a1d4dfc786c8f1ffc9626fef7
DevanshPatni05 <139652983+DevanshPatni05@users.noreply.github.com>
2025-01-08 11:01:50 +0530

    Refactor code (#3)

--- APIGateway/src/main/java/com/LostAndFound/APIGateway/controller/GoogleAuthController.java
+++ APIGateway/src/main/java/com/LostAndFound/APIGateway/controller/GoogleAuthController.java
@@ -1,17 +1,11 @@
 package com.LostAndFound.APIGateway.controller;
 
 
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.sql.PreparedStatement;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/auth/google")
