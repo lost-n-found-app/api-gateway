8d2437b936b66c2ad1a8ff621c36063fc3e85ed6
DevanshPatni05 <139652983+DevanshPatni05@users.noreply.github.com>
2025-01-15 16:10:06 +0530

    Applied authentication (#6)

---
+++ APIGateway/src/main/java/com/LostAndFound/APIGateway/configuration/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.LostAndFound.APIGateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http)
+    {
+        return http
+                .csrf(csrf->csrf.disable())
+                .authorizeExchange(auth->auth
+                        .pathMatchers("/users/**").permitAll()
+                        .anyExchange().permitAll())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+}
